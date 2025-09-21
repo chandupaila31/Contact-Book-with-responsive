c3597cc3e0b9930882a12373cebbc5ca3dbc646a
chandupaila31 <chandupaila12@gmail.com>
2025-09-21 22:46:26 +0530

    Resolve merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,9 +9,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-mongoose.connect("mongodb://localhost:27017/contactBook")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+  .catch(err => console.error("❌ MongoDB Error:", err));
+
 
 
 
